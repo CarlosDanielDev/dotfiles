48e01c6f9fe1048f3ceb6f2f5a9e17495724b68b
Carlos Daniel <carlosdanieldev@gmail.com>
2025-05-04 04:23:48 -0300

    feat: update lualine.nvim configuration and add incline.nvim plugin for improved UI and functionality

--- lua/plugins/example.lua
+++ lua/plugins/example.lua
@@ -152,27 +152,57 @@ return {
     end,
   },
 
-  -- the opts function can also be used to change the default opts:
+  -- or you can return new options to override all the defaults
+
   {
     "nvim-lualine/lualine.nvim",
     event = "VeryLazy",
-    opts = function(_, opts)
-      table.insert(opts.sections.lualine_x, {
-        function()
-          return "😄"
+    opts = {
+      options = {
+        theme = "solirized_dark",
+      },
+    },
+  },
+  {
+    "b0o/incline.nvim",
+    dependencies = { "craftzdog/solarized-osaka.nvim" },
+    event = "BufReadPre",
+    priority = 1200,
+    config = function()
+      local colors = require("solarized-osaka.colors").setup()
+      require("inclide").setup({
+        highlights = {
+          groups = {
+            InclineNormal = { guibg = colors.magenta500, guifg = colors.base04 },
+            InclineNormalNC = { guifg = colors.violet500, guibg = colors.base03 },
+          },
+        },
+        window = { margin = { vertical = 0, horizontal = 1 } },
+        hide = {
+          cursorline = true,
+        },
+        render = function(props)
+          local filename = vim.fn.fnamemodify(vim.api.nvim_buf_get_name(props.buf), ":t")
+          if vim.bo[props.buf].modified then
+            filename = "[+]" .. filename
+          end
+
+          local icon, color = require("nvim-web-devicons").get_icon_color(filename)
+
+          return {
+            {
+              icon,
+              guifg = color,
+            },
+            {
+              " ",
+            },
+            {
+              filename,
+            },
+          }
         end,
       })
-    end,
-  },
-
-  -- or you can return new options to override all the defaults
-  {
-    "nvim-lualine/lualine.nvim",
-    event = "VeryLazy",
-    opts = function()
-      return {
-        --[[add your custom lualine config here]]
-      }
     end,
   },
 
